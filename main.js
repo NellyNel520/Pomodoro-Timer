4c2eb6a254261568174f4026b803601cd795cf66
Nelly <$>
2022-11-13 16:43:29 -0500

    fixed errors

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 const timer = {
-    pomodoro: 1,
+    pomodoro: 25,
     shortBreak: 5,
     longBreak: 15,
     longBreakInterval: 4,
     sessions: 0,
   };
-
+  
   let interval;
+
+  /*Buttons that call the start timer function once pressed*/
   const buttonSound = new Audio('button-sound.mp3');
-//Buttons that call the start timer function once pressed
-const mainButton = document.getElementById('js-btn');
-mainButton.addEventListener('click', () => {
+  const mainButton = document.getElementById('js-btn');
+  mainButton.addEventListener('click', () => {
     buttonSound.play();
-  const { action } = mainButton.dataset;
-  if (action === 'start') {
-    startTimer();
-  } else {
-    stopTimer();
-  }
-});
-
+    const { action } = mainButton.dataset;
+    if (action === 'start') {
+      startTimer();
+    } else {
+      stopTimer();
+    }
+  });
+  
   const modeButtons = document.querySelector('#js-mode-buttons');
-modeButtons.addEventListener('click', handleMode);
+  modeButtons.addEventListener('click', handleMode);
 
-//takes a timestamp argument and finds the difference between the current time and the end time in milliseconds
-function getRemainingTime(endTime) {
+  /*Takes a timestamp argument and finds the difference between the current time and the end time in milliseconds*/
+  function getRemainingTime(endTime) {
     const currentTime = Date.parse(new Date());
     const difference = endTime - currentTime;
   
@@ -38,16 +39,14 @@ function getRemainingTime(endTime) {
       seconds,
     };
   }
-
-//Start Timer
-function startTimer() {
+  
+  function startTimer() {
     let { total } = timer.remainingTime;
     const endTime = Date.parse(new Date()) + total * 1000;
-
-    //sessions property is incremented at the start of a pomodoro session
+  
+    /*Sessions property is incremented at the start of a pomodoro session */
     if (timer.mode === 'pomodoro') timer.sessions++;
-
-    //button text changes to “stop” and the button becomes depressed like a hardware button
+  /*Changes button text changes to “stop” and the button becomes depressed like a hardware button */
     mainButton.dataset.action = 'stop';
     mainButton.textContent = 'stop';
     mainButton.classList.add('active');
@@ -59,26 +58,33 @@ function startTimer() {
       total = timer.remainingTime.total;
       if (total <= 0) {
         clearInterval(interval);
-
-        //Once the countdown reaches zero, the switch statement present below causes the app to switch to a new break session or pomodoro session depending on the value of timer.mode.
+  
+        /*Once the countdown reaches zero, the switch statement present below causes the app to switch to a new break session or pomodoro session depending on the value of timer.mode */
         switch (timer.mode) {
-            case 'pomodoro':
-              if (timer.sessions % timer.longBreakInterval === 0) {
-                switchMode('longBreak');
-              } else {
-                switchMode('shortBreak');
-              }
-              break;
-            default:
-              switchMode('pomodoro');
-          }
-          document.querySelector(`[data-sound="${timer.mode}"]`).play();
-    
-          startTimer();
+          case 'pomodoro':
+            if (timer.sessions % timer.longBreakInterval === 0) {
+              switchMode('longBreak');
+            } else {
+              switchMode('shortBreak');
+            }
+            break;
+          default:
+            switchMode('pomodoro');
+        }
+  
+        if (Notification.permission === 'granted') {
+          const text =
+            timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+          new Notification(text);
+        }
+  
+        document.querySelector(`[data-sound="${timer.mode}"]`).play();
+  
+        startTimer();
       }
     }, 1000);
   }
-
+  
   function stopTimer() {
     clearInterval(interval);
   
@@ -86,10 +92,8 @@ function startTimer() {
     mainButton.textContent = 'start';
     mainButton.classList.remove('active');
   }
-
-  //Update Clock function
-  /*how the countdown portion of the application is updated*/
-function updateClock() {
+  
+  function updateClock() {
     const { remainingTime } = timer;
     const minutes = `${remainingTime.minutes}`.padStart(2, '0');
     const seconds = `${remainingTime.seconds}`.padStart(2, '0');
@@ -98,15 +102,16 @@ function updateClock() {
     const sec = document.getElementById('js-seconds');
     min.textContent = minutes;
     sec.textContent = seconds;
-
-    const text = timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+  
+    const text =
+      timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
     document.title = `${minutes}:${seconds} — ${text}`;
-
+  
     const progress = document.getElementById('js-progress');
     progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
   }
-
-function switchMode(mode) {
+  
+  function switchMode(mode) {
     timer.mode = mode;
     timer.remainingTime = {
       total: timer[mode] * 60,
@@ -120,32 +125,29 @@ function switchMode(mode) {
     document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
     document.body.style.backgroundColor = `var(--${mode})`;
     document
-    .getElementById('js-progress')
-    .setAttribute('max', timer.remainingTime.total);
+      .getElementById('js-progress')
+      .setAttribute('max', timer.remainingTime.total);
   
     updateClock();
-
-
   }
-
-function handleMode(event) {
-  const { mode } = event.target.dataset;
-
-  if (!mode) return;
-
-  switchMode(mode);
-  stopTimer();
-}
-document.addEventListener('DOMContentLoaded', () => {
-    // Let's check if the browser supports notifications
+  
+  function handleMode(event) {
+    const { mode } = event.target.dataset;
+  
+    if (!mode) return;
+  
+    switchMode(mode);
+    stopTimer();
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
     if ('Notification' in window) {
-      // If notification permissions have neither been granted or denied
-      if (Notification.permission !== 'granted' && Notification.permission !== 'denied') {
-        // ask the user for permission
+      if (
+        Notification.permission !== 'granted' &&
+        Notification.permission !== 'denied'
+      ) {
         Notification.requestPermission().then(function(permission) {
-          // If permission is granted
           if (permission === 'granted') {
-            // Create a new notification
             new Notification(
               'Awesome! You will be notified at the start of each session'
             );
